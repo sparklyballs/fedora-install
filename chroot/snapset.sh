2521858abb492096b8785733768e4d7ad89ee3e6
sparklyballs <sparklyballs@gmail.com>
2024-04-19 01:08:59 +0100

    add test break

--- chroot/snapset.sh
+++ chroot/snapset.sh
@@ -17,13 +17,14 @@ dnf install -y \
 "${snapper_packages_array[@]}"
 
 # setup snapper , deleting existing folders and remounting etc.
+set -ex
 umount /.snapshots
 rm -r /.snapshots
 snapper --no-dbus -c root create-config /
-btrfs subvolume delete /.snapshots
-mkdir /.snapshots
-mount -a
+mount -va
 chmod 750 /.snapshots
+
+exit 1
 
 # configure snapper
 for conf in "${snapper_configs_array[@]}" ; do
