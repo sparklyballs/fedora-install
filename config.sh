dcb54085ca4a5596b87c61113180e327e338f406
sparklyballs <sparklyballs@gmail.com>
2024-06-14 23:36:51 +0100

    change btrfs mount options

--- config.sh
+++ config.sh
@@ -17,7 +17,7 @@ btrfs_label="FEDORA"
 mountpoint_chroot="/mnt/fedora"
 
 # mount options
-btrfs_mount_options="rw,noatime,compress-force=zstd:1,ssd,space_cache=v2,autodefrag"
+btrfs_mount_options="defaults,noatime,autodefrag,compress=zstd,commit=120"
 efi_mount_options="umask=0077,shortname=winnt"
 
 echo "$fedora_version" > /dev/null 2>&1
